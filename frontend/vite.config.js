177dcf916260014b43f5c69d706295e545cec0d0
Shrawan sah <shrawansah3067@gmail.com>
2026-04-05 03:29:40 +0530

    Render URL added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://finance-dashboard-m5sh.onrender.com',
-        changeOrigin: true
-      }
-    }
-  }
+  plugins: [react()]
 })
